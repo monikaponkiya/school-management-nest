bb7ceb19bd8de4a7d3fad2726172638ff6fbd3b6
unknown <maitri.thakkar@agileinfoways.com>
2024-05-08 17:07:45 +0530

    changes in email template and exception file

--- src/email/emailTemplates/welcome.ts
+++ src/email/emailTemplates/welcome.ts
@@ -3,6 +3,7 @@ export const welcomeTemplate = (schoolDetails, password) =>
     <!DOCTYPE html>
     <html>
         <h3>Welcome ${schoolDetails.name}</h3>
+        <br />
         <p>Please login using this password: ${password}</p>
     </html>
     `;
